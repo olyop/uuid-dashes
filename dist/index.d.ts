82a0ff309dc70ed4d48d5c848936ef8635502bd5
olyop <oliver.plummer@outlook.com>
2022-03-25 12:16:30 +1100

    Update

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const removeDashesFromUUID: (val: string) => string;
-export declare const addDashesToUUID: (x: string) => string;
--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.addDashesToUUID = exports.removeDashesFromUUID = void 0;
-const removeDashesFromUUID = (val) => val.split("-").join("");
-exports.removeDashesFromUUID = removeDashesFromUUID;
-const h = (v, f, s) => v.substr(f, s);
-const addDashesToUUID = (x) => `${h(x, 0, 8)}-${h(x, 8, 4)}-${h(x, 12, 4)}-${h(x, 16, 4)}-${h(x, 20)}`;
-exports.addDashesToUUID = addDashesToUUID;
